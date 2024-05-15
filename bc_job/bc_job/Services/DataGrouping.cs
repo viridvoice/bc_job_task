b77c96ab5752c090acc4231a3dbb744110d16b93
Vladimir Vuckovic <viridvoice@gmail.com>
2024-05-15 11:41:48 +0200

    Null checks and conditional returns

--- bc_job/bc_job/Services/DataGrouping.cs
+++ bc_job/bc_job/Services/DataGrouping.cs
@@ -5,15 +5,14 @@ namespace bc_job.Services;
 
 public class DataGrouping : IDataGrouping
 {
-    public async Task<List<List<Book>>> GroupData(List<Book> source)
+    public async Task<List<List<Book>>?> GroupData(List<Book> source)
     {
         List<List<Book>> result = new List<List<Book>>();
         var grouped = source.GroupBy(x => x.ParentName).OrderBy(x=>x.Key);
         foreach (var group in grouped) {
             result.Add(group.ToList());
         }
-        
         await Task.CompletedTask;
-        return result;
+        return result.Any() ? result : null;
     }
 }
